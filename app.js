c3af52a4fa780f15cc76c56fad90e41a853b5286
Aaron Bruce <aaronbruce555@gmail.com>
2020-01-23 11:59:04 -0500

    Updated MongoDB URL connection to a MongoDB Atlas cluster named Cluster0 and commented out previous mongoose localhost

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ var express 	= require("express"),
 	Tarot		= require("./models/tarot");
 
 // Connect to MongoDB database
-mongoose.connect('mongodb://localhost:27017/personal_website', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+// mongoose.connect('mongodb://localhost:27017/personal_website', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect('mongodb+srv://AaronBruce:<password>@cluster0-qumhf.mongodb.net/test?retryWrites=true&w=majority', { 
+	useNewUrlParser: true, 
+	useUnifiedTopology: true, 
+	useFindAndModify: false, 
+	useCreateIndex: true}).then(() => {
+		console.log('Connected to DB!');
+	}).catch(err => {
+		console.log('ERROR', err.message);
+	});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
